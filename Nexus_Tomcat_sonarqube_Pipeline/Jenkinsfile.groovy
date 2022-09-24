d9cde7b46e137cb650972e93175584715af64adc
A_Devops_Guy <98376417+iamkishore0@users.noreply.github.com>
2022-09-24 10:35:02 +0530

    Update and rename Jenkinsfile.txt to Jenkinsfile.groovy

---
+++ Nexus_Tomcat_sonarqube_Pipeline/Jenkinsfile.groovy
@@ -0,0 +1,48 @@
+
+
+pipeline {
+
+    agent any
+
+    tools {
+        maven "mvn"
+    }
+
+    stages {
+        stage('Git_clone') {
+            steps {
+                git branch: 'main', url: 'https://github.com/iamkishore0/maven_project.git'
+            }
+        }
+        stage ('Build_with_Maven') {
+            steps {
+                sh 'mvn install'
+            }
+        }
+        stage('sonar_code_analysis') {
+            environment {
+                scannerHome = tool 'sonarqube'
+            }
+            steps {
+             withSonarQubeEnv('sonarqube'){
+                 sh "${scannerHome}/bin/sonar-scanner \
+                  -Dsonar.login=57bca5c03f220d0ad4df6925468cbb2f3928a0c2 \
+                  -Dsonar.host.url=https://sonarcloud.io \
+                  -Dsonar.organization=myorganization1234 \
+                  -Dsonar.projectKey=myorganization1234 \
+                  -Dsonar.java.binaries=./ "
+                }
+            }
+        }
+        stage ('Nexus_Artifact_uploader') {
+            steps {
+                nexusArtifactUploader artifacts: [[artifactId: '$BUILD_TIMESTAMP', classifier: '', file: '/var/lib/jenkins/workspace/Complete_Pipeline/webapp/target/webapp.war', type: 'war']], credentialsId: 'nexus_tomcat', groupId: 'srinivas', nexusUrl: '54.226.245.6:8081/nexus', nexusVersion: 'nexus2', protocol: 'http', repository: 'srinivas', version: '$BUILD_ID'
+            }
+        }
+        stage ('Deploy_to_container') {
+            steps {
+                deploy adapters: [tomcat8(credentialsId: 'nexus_tomcat', path: '', url: 'http://54.236.101.249:8080')], contextPath: null, war: '**/*.war'
+            }
+        }
+    }
+}
